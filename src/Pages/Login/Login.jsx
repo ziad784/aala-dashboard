f66a1da0c73f13cdf01f5cb7c54cc3c258e68822
ziad <ziad1090mohamed@gmail.com>
2022-05-12 19:46:20 +0200

    first push

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -24,21 +24,21 @@ function Login() {
       })
     })
     .then((res) => res.json())
-    .then((data) =>{
+    .then(async (data) =>{
 
       if(data.res === "bad"){
         const login_form = document.getElementById("login_form");
         const err = document.createElement("div");
         err.className = "err show_err_anim";
         err.textContent = data.msg;
-        console.log(login_form.children[0]);
+     
         if(!login_form.children[0].classList.contains("err")){
           login_form.prepend(err);
         }
 
       }else if(data.res === "ok"){
 
-        localStorage.setItem("DashToken",data.token)
+         localStorage.setItem("DashToken",data.token)
         
         setTimeout(() => {
           Navigate("/",{
@@ -66,7 +66,7 @@ function Login() {
         <div className="login_form" id='login_form'>
 
         <Input txt={"البريد الالكتروني"} type={"text"} onchange={(e)=>{setEmail(e.target.value);}} />
-          <Input txt={"كلمة المرور"} type={"password"} onchange={(e)=>{setPassword(e.target.value);}} />
+          <Input txt={"كلمة السر"} type={"password"} onchange={(e)=>{setPassword(e.target.value);}} />
           <Button txt={"تسجيل دخول"} color={"#101010"}  width={"90%"} onclick={LoginHandler}  />
         </div>
 
